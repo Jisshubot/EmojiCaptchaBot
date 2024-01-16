83c05f4a1fdae170adb8589716c67755db0b7fd6
Zishan khan <142872501+Jisshubot@users.noreply.github.com>
2024-01-16 18:57:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", 20681593))
+    API_ID = int(os.environ.get("API_ID", "20681593"))
     API_HASH = os.environ.get("API_HASH", "379596c99399dffbf5cd00f1242ec60c")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6733186557:AAEZ_fLnfT3N3Dq4Mxyl_FFs6S0kuTTSLyo")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6397703693:AAGP8nhnX-HnppTe1UiaJXZiPpXSXSRTVKc")
     SESSION_NAME = os.environ.get("SESSION_NAME", "Captcha-Bot")
-    GROUP_CHAT_ID = int(os.environ.get("GROUP_CHAT_ID", -1001665155031))
+    GROUP_CHAT_ID = int(os.environ.get("GROUP_CHAT_ID", -1002102292245))
